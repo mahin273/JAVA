e22abafa4ec99ebf0f76945a69c728ccbc267450
mahin273 <md.mahin.bd18@gmail.com>
2023-08-15 22:22:36 +0600

    Temperature Converter using java swing

---
+++ Temperature Converter/src/TemperatureConverter.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TemperatureConverter extends JFrame {
+    private JTextField inputField;
+    private JLabel resultLabel;
+
+    public TemperatureConverter() {
+        // Set up the JFrame
+        setTitle("Temperature Converter");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        // Create components
+        JLabel inputLabel = new JLabel("Enter temperature:");
+        inputField = new JTextField(10);
+        JButton convertButton = new JButton("Convert");
+        resultLabel = new JLabel();
+
+        // Add components to the JFrame
+        add(inputLabel);
+        add(inputField);
+        add(convertButton);
+        add(resultLabel);
+
+        // Handle events
+        convertButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double inputTemperature = Double.parseDouble(inputField.getText());
+
+                    // Convert temperature
+                    double celsius = (inputTemperature - 32) * 5 / 9;
+                    double kelvin = celsius + 273.15;
+                    double fahrenheit = (inputTemperature * 9 / 5) + 32;
+
+                    // Display results
+                    String result = "Celsius: " + celsius + "°C\n"
+                            + "Kelvin: " + kelvin + "K\n"
+                            + "Fahrenheit: " + fahrenheit + "°F";
+                    resultLabel.setText(result);
+                } catch (NumberFormatException ex) {
+                    resultLabel.setText("Invalid input");
+                }
+            }
+        });
+
+        // Set visibility
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TemperatureConverter();
+            }
+        });
+    }
+}
